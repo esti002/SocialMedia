572774e3864444522672c48ce645d4152ff17a0a
esti002 <85763881+esti002@users.noreply.github.com>
2022-03-18 23:45:41 +0300

    create projects

---
+++ SocialMediaWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialMediaWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
