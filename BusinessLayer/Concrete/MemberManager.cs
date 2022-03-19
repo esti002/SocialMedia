b34cba130ede6eff4138c8c643dab19db5f9b718
esti002 <85763881+esti002@users.noreply.github.com>
2022-03-19 21:15:42 +0300

    create business layer

---
+++ BusinessLayer/Concrete/MemberManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.MSSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MemberManager : IMamberService
+    {
+        EfMemberRepo memberRepo;
+
+        public MemberManager()
+        {
+            memberRepo = new EfMemberRepo();
+        }
+
+        public void GenericAdd(Member t)
+        {
+            memberRepo.Insert(t);
+        }
+
+        public void GenericDelete(Member t)
+        {
+            memberRepo.Delete(t);
+        }
+
+        public void GenericUpdate(Member t)
+        {
+            memberRepo.Update(t);
+        }
+
+        public Member GetById(int id)
+        {
+            return memberRepo.GetById(id);
+        }
+
+        public List<Member> GetList()
+        {
+            return memberRepo.GetAll();
+        }
+    }
+}
